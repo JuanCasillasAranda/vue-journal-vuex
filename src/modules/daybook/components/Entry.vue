f3df6ebb26788add08c95096a4d1325faac9f0ec
Juan Manuel Casillas Aranda <juan.casillas@ctransporta.com>
2022-06-13 21:22:07 -0500

    Primer commit

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class=" entry-cointainer mb-3 pointer p-2"
+    @click="$router.push({name: 'entry', params:{ id:10}})"
+  >
+    <!-- Titulo -->
+    <div class="entry-title d-flex">
+        <span class="text-success fs-5 fw-bold">12</span>
+        <span class="mx-1 fs-5">Junio</span>
+        <span class="mx-2 fw-light">2022 Domingo</span>
+    </div>
+
+    <div class="entry-description">
+        lorem lorem consectetur ipsum adipisicing ipsum adipisicing sit consectetur sit elit consectetur dolor elit ipsum adipisicing sit sit elit. sit adipisicing adipisicing amet amet dolor ipsum dolor sit ipsum amet consectetur ipsum sit consectetur dolor sit amet lorem ipsum. amet ipsum lorem elit dolor ipsum ipsum ipsum ipsum dolor sit amet ipsum amet amet adipisicing elit adipisicing sit elit. amet elit elit adipisicing amet lorem lorem adipisicing adipisicing dolor amet ipsum lorem ipsum consectetur adipisicing ipsum lorem. ipsum ipsum lorem dolor adipisicing elit adipisicing dolor sit amet amet amet amet amet elit dolor.
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-cointainer{
+    border-bottom:1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+    
+    &:hover{
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+
+    .entry-description {
+        font-size: 12px;
+    }
+}
+
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries:[
+        {
+            id: new Date().getTime(), 
+            date: new Date().toDateString(),
+            text: 'dolor lorem amet elit dolor dolor consectetur dolor adipisicing elit sit adipisicing sit adipisicing elit amet elit lorem lorem. dolor dolor ipsum lorem consectetur adipisicing adipisicing ipsum sit consectetur elit sit lorem lorem lorem ipsum sit amet amet. elit amet sit lorem amet ipsum elit elit amet dolor lorem dolor sit amet lorem elit ipsum ipsum. adipisicing amet adipisicing adipisicing consectetur dolor ipsum ipsum dolor lorem ipsum dolor lorem lorem adipisicing lorem ipsum lorem. sit ipsum amet ipsum dolor consectetur adipisicing ipsum sit sit sit consectetur consectetur ipsum consectetur lorem elit lorem elit sit consectetur. consectetur consectetur elit elit consectetur adipisicing elit amet elit ipsum ipsum dolor dolor lorem adipisicing elit.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000, 
+            date: new Date().toDateString(),
+            text: 'lorem dolor consectetur amet sit lorem ipsum elit elit lorem amet adipisicing adipisicing elit adipisicing consectetur adipisicing lorem elit. amet elit consectetur dolor adipisicing lorem amet elit ipsum amet sit dolor consectetur consectetur sit sit. adipisicing consectetur sit sit lorem dolor dolor adipisicing sit lorem amet dolor ipsum elit amet elit consectetur consectetur adipisicing consectetur. adipisicing elit elit amet lorem ipsum amet sit consectetur lorem sit adipisicing lorem adipisicing elit adipisicing sit amet. consectetur lorem amet lorem amet consectetur dolor amet adipisicing consectetur amet sit adipisicing elit elit consectetur lorem sit sit. lorem dolor dolor consectetur consectetur sit elit elit ipsum consectetur elit sit consectetur sit dolor adipisicing elit.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000, 
+            date: new Date().toDateString(),
+            text: 'adipisicing ipsum lorem sit adipisicing sit ipsum dolor dolor amet consectetur consectetur adipisicing elit lorem adipisicing dolor ipsum. sit lorem dolor sit consectetur sit consectetur consectetur consectetur ipsum amet consectetur adipisicing amet adipisicing elit. dolor amet consectetur elit adipisicing ipsum ipsum adipisicing adipisicing consectetur lorem amet adipisicing lorem sit amet. lorem consectetur sit sit ipsum adipisicing elit adipisicing sit elit consectetur dolor dolor sit dolor dolor amet. dolor consectetur ipsum amet ipsum elit dolor adipisicing dolor sit ipsum ipsum amet sit elit amet ipsum adipisicing.',
+            picture: null,
+        }
+    ]
+})
